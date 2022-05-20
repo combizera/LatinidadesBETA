637e39f1953537d5cc3ea29e1752335f2e9fcb8d
Ygor Combi <ycombi349@gmail.com>
2022-05-20 09:07:39 -0300

    inicio mobile main

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-const ativar = document.querySelector(".sec1__cta");
+const btnMobile = document.getElementById("btn__mobile");
 
-function handleClick(event) {
-  console.log(`Clicou em: ${ativar}.`);
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("cabecalho__nav");
+  nav.classList.toggle("active");
+
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
 }
 
-ativar.addEventListener("click", handleClick);
-console.log(ativar);
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
+
+function inscreveu() {
+  var name = document.getElementById("inputName").value;
+  var email = document.getElementById("inputEmail").value;
+
+  if (name && email != "") {
+    window.alert(`Obrigado por se inscrever ${name}!`);
+  } else {
+    window.alert("[ERRO] Preencha corretamente o formulário amigão!");
+  }
+}
+// TESTE
+// const ativar = document.querySelector(".sec1__cta");
+
+// function handleClick(event) {
+//   console.log(`Clicou em: ${ativar}.`);
+// }
+
+// ativar.addEventListener("click", handleClick);
+// console.log(ativar);
